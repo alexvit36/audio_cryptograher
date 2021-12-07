a3ec0262f0826058b080b0cf6eb69c9597ffdffd
alexvit36 <56106373+alexvit36@users.noreply.github.com>
2021-12-07 15:48:49 +0300

    Create generator of the keys
    pre-release version

---
+++ generate_keys.py
@@ -0,0 +1,7 @@
+import rsa
+
+public, private = rsa.newkeys(512)
+with open("private_key.txt", "w") as pr:
+    pr.write("{} {} {} {} {}".format(private.n, private.e, private.d, private.p, private.q))
+with open("public_key.txt", "w") as pub:
+    pub.write("{} {}".format(public.n, public.e))
