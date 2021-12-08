4af5ad60da1f2c3b1722f6f42f6f872efccf2048
Aleksandr Fedotov <56106373+alexvit36@users.noreply.github.com>
2021-12-09 00:40:17 +0300

    updates according to the new requirements

--- decoder.py
+++ decoder.py
@@ -3,11 +3,11 @@ import argparse
 from Audio import Audio
 import rsa
 from Steganographer import Steganographer
-from BBS import BBS
 
 
 parser = argparse.ArgumentParser(description='Encryptor parser')
 parser.add_argument('-i', type=str, default=r'mozart_after.wav')
+parser.add_argument('-enc', type=str, default="utf-8")
 
 with open("private_key.txt", "r") as pr:
     private_str = pr.read().split(" ")
@@ -17,4 +17,4 @@ args = parser.parse_args()
 audio = Audio(filename=args.i)
 decoder = Steganographer(audio=audio)
 res = decoder.decode(private)
-print(res)
+print(res.replace("*", "\n"))
